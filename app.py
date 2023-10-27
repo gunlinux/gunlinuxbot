37fb8e7c7d75c29435497cb16cef2db9446607b7
loki <gunlinux@ya.ru>
2023-10-27 22:31:04 +1000

    basic mssg handler

---
+++ app.py
@@ -0,0 +1,72 @@
+from models.user import User
+from models.mssg import Mssg
+from models.command import Command
+
+
+class EventHandler:
+    users: set[User] = set()
+    mssgs: list[Mssg] = list()
+    commands: dict = dict()
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return f'{self.users} {self.mssgs}'
+
+    def get_or_add_user(self, id, username):
+        for user in self.users:
+            if user.id == id:
+                return user
+        user = User(id=id, username=username)
+        self.users.add(user)
+        return user
+
+    def handle_event(self, message):
+        user = self.get_or_add_user(message['userid'], message['username'])
+        mssg = Mssg(id=message['id'], mssg=message['mssg'], user_id=user.id)
+        user.new_mssg()
+        self.mssgs.append(mssg)
+        print(f'{user.username}: {mssg.mssg}')
+        for command_name, command in self.commands.items():
+            if message['mssg'].startswith(command_name):
+                print(f'detected command: {command}')
+                command.run(mssg, user)
+
+    def show_users(self):
+        print('users:')
+        for user in self.users:
+            print(user)
+
+    def register(self, name, Command):
+        print(f'successfully registed command {name}')
+        self.commands[name] = Command
+        print(self.commands)
+
+    def chat(self, mssg):
+        print(f'send_mssg_to_chat {mssg}')
+
+
+def auf(message, user, event_hander=None):
+    if event_handler:
+        event_handler.chat(f'@{user.username} Воистину ауф')
+
+
+if __name__ == '__main__':
+    event_handler = EventHandler()
+    echo_command = Command('!ауф',  event_handler, real_runner=auf)
+    messages = [
+        {'userid': 1, 'username': 'gunlinux', 'mssg': 'hello', 'id': 1},
+        {'userid': 2, 'username': 'onehundredprecentass', 'mssg': '!ауф', 'id': 2},
+        {'userid': 1, 'username': 'gunlinux', 'mssg': 'сяп', 'id': 3},
+        {'userid': 1, 'username': 'gunlinux', 'mssg': '!ауф', 'id': 4},
+        {'userid': 1, 'username': 'gunlinux', 'mssg': '!echo hello', 'id': 4},
+        {'userid': 3, 'username': 'wigust', 'mssg': 'Pog', 'id': 5},
+    ]
+
+
+    for message in messages:
+        event_handler.handle_event(message)
+
+    event_handler.show_users()
+
---
+++ models/command.py
@@ -0,0 +1,21 @@
+class Command:
+    event_handler: None
+    name: str
+    real_runner: None
+
+    def __init__(self, name, event_handler, real_runner=None):
+        self.name = name
+        self.event_handler = event_handler
+        self.event_handler.register(self.name, self)
+        self.real_runner = real_runner
+
+    def run(self, message, user):
+        print(f'run command {self.name}')
+        if self.real_runner is None:
+            print('not implemented yet')
+            return
+        return self.real_runner(message, user, self.event_handler)
+
+    def __str__(self):
+        return f'<Command> {self.name}'
+
