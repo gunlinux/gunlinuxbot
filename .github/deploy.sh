686aaead6f35f0b0710582e959dfb0f8676240a0
loki <gunlinux@ya.ru>
2025-07-25 02:44:39 +1000

    deploy fix

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -16,10 +16,8 @@ git reset --hard origin/$BRANCH
 # Restart services using sudo
 sudo systemctl restart donats_getter
 sudo systemctl restart donats_worker
-sudo systemctl restart twitch_getter
 sudo systemctl restart retwitch_getter
 sudo systemctl restart twitch_sender
-sudo systemctl restart twitch_worker
 sudo systemctl restart retwitch_worker
 sudo systemctl restart beer_consumer
 
