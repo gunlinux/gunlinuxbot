0ff9f51567f550c87910f1950f6446e5a2f75ce8
loki <gunlinux@ya.ru>
2025-08-13 04:44:24 +1000

    code review changes

--- donats/models.py
+++ donats/models.py
@@ -1,21 +1,8 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 
 import typing
 
 from gunlinuxbot.models import Event
-
-
-"""
-class DonationAlertTypes(Enum):
-    DONATION = 1
-    CUSTOM_REWARD = 19
-    FOLLOW = 6
-
-
-class DonationTypes(Enum):
-    DONATION = '1'
-    REWARD = '19'
-"""
 
 
 @dataclass
@@ -31,18 +18,6 @@ class AlertEvent(Event):
     # valdate as date???
     date_created: str
     _is_test_alert: bool
-    # Method to serialize the dataclass instance into JSON-compatible dict
 
     def serialize(self) -> dict[str, typing.Any]:
-        return {
-            'id': self.id,
-            'alert_type': self.alert_type,
-            'billing_system': self.billing_system,
-            'username': self.username,
-            'amount': self.amount,
-            'amount_formatted': self.amount_formatted,
-            'currency': self.currency,
-            'message': self.message,
-            'date_created': self.date_created,  # Assuming date is already in string format
-            '_is_test_alert': self._is_test_alert,
-        }
+        return asdict(self)
